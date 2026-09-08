16caf84eaf3678377e5b5d002e5ab3e61ebe0e17
Elzo Brito <elzo.santos@cps.sp.gov.br>
2026-09-08 20:06:14 -0300

    new file:   usabilidade-web-mobile-jogos/2026-2/atividades/0809/CadastroUsuariosBtn.java

---
+++ usabilidade-web-mobile-jogos/2026-2/atividades/0809/CadastroUsuariosBtn.java
@@ -0,0 +1,27 @@
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class CadastroUsuariosBtn {
+    public static void main(String[] args) {
+        Frame janela = new Frame("Cadastro de usuários");
+        janela.setLayout(new FlowLayout());
+
+        Label rotulo = new Label("Nenhum clique ainda");
+        Button botao = new Button("Clicar");
+        janela.add(rotulo);
+        janela.add(botao);
+        janela.setSize(480, 240);
+        janela.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent evento) {
+                janela.dispose();
+                System.exit(0);
+            }
+        });
+        janela.setVisible(true);
+    }
+}
